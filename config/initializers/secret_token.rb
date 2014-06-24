1338de74f2cf6f71d36e1dbfe39dcf3ba2846656
Samantha Brooks <sbrooks9@mix.wvu.edu>
2014-06-24 19:06:20 -0400

    Intial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '5954217a2ad30e2f5033d7faeae40492f159502c6010a882392e599e2e5a82a48fa86a27882bb20828c903887e91af360aea16dea9fcb864418f4e8b4f496e31'
