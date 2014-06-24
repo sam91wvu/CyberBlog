bc6775a16c2a510f2a57890278f1abf21018ee13
Samantha Brooks <sbrooks9@mix.wvu.edu>
2014-06-24 19:59:40 -0400

    halfway done

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Blog::Application.routes.draw do
+  resources :posts do
+    resources :comments
+end
+
+
+root :to => "home#index"
+
+  get "home/index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
